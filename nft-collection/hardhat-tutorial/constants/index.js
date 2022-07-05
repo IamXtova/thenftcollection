770d5ea354479eac299becf7fadb3712d7a30a4f
IamXtova <christophe.onuorah@gmail.com>
2022-07-05 22:53:06 +0100

    adding files

---
+++ nft-collection/hardhat-tutorial/constants/index.js
@@ -0,0 +1,6 @@
+// Address of the Whitelist Contract that you deployed
+const WHITELIST_CONTRACT_ADDRESS = "0x4dda5011b73b2c4714fc1b578a1203ab61169810";
+// URL to extract Metadata for a Crypto Dev NFT
+const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+
+module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
